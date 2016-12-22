ec747be258f195bda372c47cd052d16c2bd20702
Vanna <lsphunterman@gmail.com>
2016-12-22 09:23:59 +0700

    1111

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -6,8 +6,4 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Close()
     End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        TextBox1.Text = "Hello GitHub"
-    End Sub
 End Class
